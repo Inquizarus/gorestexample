08e2dffe2daefd767c96e6eb415d01905415863c
Conny Karlsson <conny@pop-os.localdomain>
2020-03-23 23:04:51 +0100

    updated to new gorest version

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/inquizarus/gorestexample
 
 go 1.14
 
-require github.com/inquizarus/gorest v0.2.0
+require github.com/inquizarus/gorest v0.2.1
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -8,9 +9,15 @@ import (
 	gorest "github.com/inquizarus/gorest"
 )
 
+// Response is a basic response structure that can
+// be Marshaled into bytes for writing in a ResponseWriter
+type Response struct {
+	Message string `json:"message"`
+}
+
 func main() {
 	tlsConfig := gorest.ServeTLSConfig{
-		Enabled:          false,
+		Enabled:          true,
 		Strict:           false,
 		Preload:          false,
 		StrictSubDomains: false,
@@ -31,12 +38,19 @@ func main() {
 		Handlers: []gorest.Handler{
 			&gorest.BaseHandler{
 				Path: "/",
-				Get: func(w http.ResponseWriter, r *http.Request, p map[string]string) {
-					defer r.Body.Close()
-					w.Write([]byte(`{"message": "Hello, World!"}`))
-				},
+				Get:  makeGetHandler("Hello, World!"),
 			},
 		},
 	}
 	gorest.Serve(config)
+}
+
+func makeGetHandler(reply string) gorest.VerbHandler {
+	return func(w http.ResponseWriter, r *http.Request, p map[string]string) {
+		var response Response
+		defer r.Body.Close()
+		response.Message = reply
+		bodyBytes, _ := json.Marshal(response)
+		w.Write([]byte(bodyBytes))
+	}
 }
